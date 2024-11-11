openshift/ib-sriov-cni

commit 625f73b517722321865f6a1084167636f2e8fc6c

d/s only - build for RHEL8 and RHEL9
During upgrades, it can happen that the newer version
of the CNI plugin, which is built on RHEL9, run on a RHEL8
system. As the `CGO_ENABLED` is forced to 1 due to the FIPS
compliance, these situations lead to a `GLIBC_2.32' not found ...`
error.

Make `Dockerfile.rhel7` build binaries for both RHEL8 and RHEL9
Change default source path of `images/entrypoint.sh` to deploy the
right build of the binary, based on the `/host/etc/os-release` file, if present.

Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

images/entrypoint.sh
@@ -3,11 +3,6 @@
 # Always exit on errors.
 set -e
 
-# Set known directories.
-CNI_BIN_DIR="/host/opt/cni/bin"
-IB_SRIOV_CNI_BIN_FILE="/usr/bin/ib-sriov"
-NO_SLEEP=0
-
 # Give help text for parameters.
 usage()
 {
@@ -21,6 +16,57 @@ usage()
     printf "\t--ib-sriov-cni-bin-file=%s\n" "$IB_SRIOV_CNI_BIN_FILE"
     printf "\t--no-sleep\n"
 }
+
+get_source_folder_for_rhel_version()
+{
+    if [ ! -f /host/etc/os-release ]; then
+        echo "/usr/bin"
+        return
+    fi
+
+    # shellcheck source=/dev/null
+    . /host/etc/os-release
+    
+    rhelmajor=
+    # detect which version we're using in order to copy the proper binaries
+    case "${ID}" in
+        rhcos|scos)
+            rhelmajor=$(echo "$RHEL_VERSION" | sed -E 's/([0-9]+)\.{1}[0-9]+(\.[0-9]+)?/\1/')
+        ;;
+        rhel) rhelmajor=$(echo "${VERSION_ID}" | cut -f 1 -d .)
+        ;;
+        fedora)
+            if [ "${VARIANT_ID}" = "coreos" ]; then
+            rhelmajor=8
+            else
+            log "FATAL ERROR: Unsupported Fedora variant=${VARIANT_ID}"
+            exit 1
+            fi
+        ;;
+        *) log "FATAL ERROR: Unsupported OS ID=${ID}"; exit 1
+        ;;
+        esac
+        # Set which directory we'll copy from, detect if it exists
+        sourcedir=/usr/bin
+        case "${rhelmajor}" in
+        8)
+        sourcedir=/usr/bin/rhel8
+        ;;
+        9)
+        sourcedir=/usr/bin/rhel9
+        ;;
+        *)
+        log "ERROR: RHEL Major Version Unsupported, rhelmajor=${rhelmajor}"
+        ;;
+    esac
+
+    echo "${sourcedir}"
+}
+
+# Set known directories.
+CNI_BIN_DIR="/host/opt/cni/bin"
+IB_SRIOV_CNI_BIN_FILE="$(get_source_folder_for_rhel_version)/ib-sriov"
+NO_SLEEP=0
 
 # Parse parameters given as arguments to this script.
 while [ "$1" != "" ]; do

Dockerfile.rhel7
@@ -1,4 +1,14 @@
-FROM registry.ci.openshift.org/ocp/builder:rhel-9-golang-1.21-openshift-4.16 AS builder
+FROM registry.ci.openshift.org/ocp/builder:rhel-8-golang-1.21-openshift-4.16 AS builder-rhel8
+
+COPY . /usr/src/ib-sriov-cni
+
+WORKDIR /usr/src/ib-sriov-cni
+RUN make clean && \
+    make build
+
+
+FROM registry.ci.openshift.org/ocp/builder:rhel-9-golang-1.21-openshift-4.16 AS builder-rhel9
+
 
 COPY . /usr/src/ib-sriov-cni
 
@@ -7,7 +17,14 @@ RUN make clean && \
     make build
 
 FROM registry.ci.openshift.org/ocp/4.16:base-rhel9
-COPY --from=builder /usr/src/ib-sriov-cni/build/ib-sriov /usr/bin/
+
+COPY --from=builder-rhel9 /usr/src/ib-sriov-cni/build/ib-sriov /usr/bin/
+
+RUN mkdir /usr/bin/rhel9
+COPY --from=builder-rhel9 /usr/src/ib-sriov-cni/build/ib-sriov /usr/bin/rhel9
+RUN mkdir /usr/bin/rhel8
+COPY --from=builder-rhel8 /usr/src/ib-sriov-cni/build/ib-sriov /usr/bin/rhel8
+
 WORKDIR /
 
 LABEL io.k8s.display-name="InfiniBand SR-IOV CNI"

images/image_test_rhel.sh
@@ -1,0 +1,33 @@
+#!/bin/bash
+
+set -x
+
+OCI_RUNTIME=$1
+IMAGE_UNDER_TEST=sriov-test
+
+"${OCI_RUNTIME}" build -t "${IMAGE_UNDER_TEST}" -f ./Dockerfile.rhel7 .
+
+OUT_DEFAULT=$(mktemp -d)
+OUT_RHEL9=$(mktemp -d)
+OUT_RHEL8=$(mktemp -d)
+TESTDATA="$(dirname -- "${BASH_SOURCE[0]}")"/testdata
+
+
+"${OCI_RUNTIME}" run -v "${OUT_DEFAULT}:/out" "${IMAGE_UNDER_TEST}" --cni-bin-dir=/out --no-sleep
+"${OCI_RUNTIME}" run -v "${TESTDATA}/rhel8-etc-os-release:/host/etc/os-release" -v "${OUT_RHEL8}:/out" "${IMAGE_UNDER_TEST}" --cni-bin-dir=/out --no-sleep
+"${OCI_RUNTIME}" run -v "${TESTDATA}/rhel9-etc-os-release:/host/etc/os-release" -v "${OUT_RHEL9}:/out" "${IMAGE_UNDER_TEST}" --cni-bin-dir=/out --no-sleep
+
+if ! "${OCI_RUNTIME}"  run -e CNI_COMMAND=VERSION -v "${OUT_RHEL8}/ib-sriov:/ib-sriov" registry.access.redhat.com/ubi8/ubi /ib-sriov ; then
+    echo "RHEL8 binary does not run on ubi8 image"
+    exit 1
+fi
+
+if ! "${OCI_RUNTIME}"  run -e CNI_COMMAND=VERSION -v "${OUT_RHEL9}/ib-sriov:/ib-sriov" registry.access.redhat.com/ubi9/ubi /ib-sriov ; then
+    echo "RHEL9 binary does not run on ubi9 image"
+    exit 1
+fi
+
+cmp "${OUT_DEFAULT}/ib-sriov" "${OUT_RHEL9}/ib-sriov" || { echo "default and RHEL9 binaries differ"; exit 1; }
+
+exit 0
+

images/testdata/rhel8-etc-os-release
@@ -1,0 +1,18 @@
+NAME="Red Hat Enterprise Linux CoreOS"
+ID="rhcos"
+ID_LIKE="rhel fedora"
+VERSION="412.92.202402061222-0"
+VERSION_ID="4.12"
+VARIANT="CoreOS"
+VARIANT_ID=coreos
+PLATFORM_ID="platform:el8"
+PRETTY_NAME="Red Hat Enterprise Linux CoreOS 415.92.202402061222-0 (Plow)"
+HOME_URL="https://www.redhat.com/"
+DOCUMENTATION_URL="https://docs.openshift.com/container-platform/4.12/"
+BUG_REPORT_URL="https://bugzilla.redhat.com/"
+REDHAT_BUGZILLA_PRODUCT="OpenShift Container Platform"
+REDHAT_BUGZILLA_PRODUCT_VERSION="4.12"
+REDHAT_SUPPORT_PRODUCT="OpenShift Container Platform"
+REDHAT_SUPPORT_PRODUCT_VERSION="4.12"
+OPENSHIFT_VERSION="4.12"
+RHEL_VERSION="8.4"

images/testdata/rhel9-etc-os-release
@@ -1,0 +1,21 @@
+NAME="Red Hat Enterprise Linux CoreOS"
+ID="rhcos"
+ID_LIKE="rhel fedora"
+VERSION="415.92.202402061222-0"
+VERSION_ID="4.15"
+VARIANT="CoreOS"
+VARIANT_ID=coreos
+PLATFORM_ID="platform:el9"
+PRETTY_NAME="Red Hat Enterprise Linux CoreOS 415.92.202402061222-0 (Plow)"
+ANSI_COLOR="0;31"
+CPE_NAME="cpe:/o:redhat:enterprise_linux:9::coreos"
+HOME_URL="https://www.redhat.com/"
+DOCUMENTATION_URL="https://docs.openshift.com/container-platform/4.15/"
+BUG_REPORT_URL="https://bugzilla.redhat.com/"
+REDHAT_BUGZILLA_PRODUCT="OpenShift Container Platform"
+REDHAT_BUGZILLA_PRODUCT_VERSION="4.15"
+REDHAT_SUPPORT_PRODUCT="OpenShift Container Platform"
+REDHAT_SUPPORT_PRODUCT_VERSION="4.15"
+OPENSHIFT_VERSION="4.15"
+RHEL_VERSION="9.2"
+OSTREE_VERSION="415.92.202402061222-0"

commit 7a04577ec1174e2de6a90a4958068c94ebcc1ccb

d/s only - adjust check for os-release on CentOS Stream CoreOS (csco)
CSCO does not define "$RHEL_VERSION" in "/etc/os-release" (anymore?).
This leads to a failure to detect the OS and an error

   ERROR: RHEL Major Version Unsupported, rhelmajor=

Instead, fallback to "$PLATFORM_ID".

Also, use "echo" instead of "log". No such command exists.

https://issues.redhat.com/browse/OCPBUGS-44313

Fixes: 625f73b51772 ('d/s only - build for RHEL8 and RHEL9')

images/entrypoint.sh
@@ -32,6 +32,9 @@ get_source_folder_for_rhel_version()
     case "${ID}" in
         rhcos|scos)
             rhelmajor=$(echo "$RHEL_VERSION" | sed -E 's/([0-9]+)\.{1}[0-9]+(\.[0-9]+)?/\1/')
+            if [ -z "$rhelmajor" -a "$ID" = scos ] ; then
+                rhelmajor="$(echo "$PLATFORM_ID" | sed -E 's/^platform:el([0-9]+)$/\1/')"
+            fi
         ;;
         rhel) rhelmajor=$(echo "${VERSION_ID}" | cut -f 1 -d .)
         ;;
@@ -39,11 +42,11 @@ get_source_folder_for_rhel_version()
             if [ "${VARIANT_ID}" = "coreos" ]; then
             rhelmajor=8
             else
-            log "FATAL ERROR: Unsupported Fedora variant=${VARIANT_ID}"
+            echo "FATAL ERROR: Unsupported Fedora variant=${VARIANT_ID}"
             exit 1
             fi
         ;;
-        *) log "FATAL ERROR: Unsupported OS ID=${ID}"; exit 1
+        *) echo "FATAL ERROR: Unsupported OS ID=${ID}"; exit 1
         ;;
         esac
         # Set which directory we'll copy from, detect if it exists
@@ -56,7 +59,7 @@ get_source_folder_for_rhel_version()
         sourcedir=/usr/bin/rhel9
         ;;
         *)
-        log "ERROR: RHEL Major Version Unsupported, rhelmajor=${rhelmajor}"
+        echo "ERROR: RHEL Major Version Unsupported, rhelmajor=${rhelmajor}"
         ;;
     esac
 